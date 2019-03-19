868a2639800f6de9ff927f03711d5341923a2c26
Martin Baulig <mabaul@microsoft.com>
2019-03-19 09:43:20 -0400

    X

--- Tests/test-globalization-1.cs
+++ Tests/test-globalization-1.cs
@@ -7,8 +7,16 @@ namespace Martin.LinkerTest
 	{
 		public static void Main ()
 		{
+			if (MonoLinkerSupport.IsTypeAvailable ("Mono.Globalization.Unicode.SimpleCollator"))
+				throw new AssertionException ("Mono.Globalization.Unicode.SimpleCollator");
 			if (MonoLinkerSupport.IsTypeAvailable ("System.Globalization.JapaneseCalendar"))
-				throw new AssertionException ("System.Globalization.JapaneseCalendar.");
+				throw new AssertionException ("System.Globalization.JapaneseCalendar");
+
+			var now = DateTime.Now;
+			Console.WriteLine (DateTime.Now);
+			var parsed = DateTime.Parse (now.ToString ());
+			if (now.ToString () != parsed.ToString ())
+				throw new AssertionException ();
 		}
 	}
 }
