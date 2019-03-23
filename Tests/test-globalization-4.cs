1b608bf88e3f7ea0edd441d1bf6e104f8fe7cf62
Martin Baulig <mabaul@microsoft.com>
2019-03-23 01:06:48 -0400

    X

---
+++ Tests/test-globalization-4.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Martin.LinkerTest
+{
+	static class TestGlobalization
+	{
+		public static void Main ()
+		{
+			Console.WriteLine ("Hello!");
+
+			var now = DateTime.Now;
+			Console.WriteLine (DateTime.Now);
+		}
+	}
+}
