5aac359b39efa7a8ed7bfb607ef335485ffca633
Martin Baulig <mabaul@microsoft.com>
2019-04-02 18:25:17 -0400

    X

--- Program.cs
+++ Program.cs
@@ -124,14 +124,14 @@ namespace Mono.Linker.Optimizer
 		{
 			while (arguments.Count > 0) {
 				var token = arguments[0];
-				if (!token.StartsWith ("--martin", StringComparison.Ordinal))
+				if (!token.StartsWith ("--optimizer", StringComparison.Ordinal))
 					break;
 
 				arguments.RemoveAt (0);
 				switch (token) {
-				case "--martin":
+				case "--optimizer":
 					if (mainModule != null) {
-						Console.Error.WriteLine ($"Duplicate --martin argument.");
+						Console.Error.WriteLine ($"Duplicate --optimizer argument.");
 						Environment.Exit (1);
 					}
 					mainModule = arguments[0];
@@ -139,18 +139,18 @@ namespace Mono.Linker.Optimizer
 					LoadFile (mainModule);
 					moduleEnabled = true;
 					continue;
-				case "--martin-xml":
+				case "--optimizer-xml":
 					var filename = arguments[0];
 					arguments.RemoveAt (0);
 					OptionsReader.Read (options, filename);
 					moduleEnabled = true;
 					break;
-				case "--martin-options":
+				case "--optimizer-options":
 					options.ParseOptions (arguments[0]);
 					arguments.RemoveAt (0);
 					moduleEnabled = true;
 					break;
-				case "--martin-report":
+				case "--optimizer-report":
 					filename = arguments [0];
 					arguments.RemoveAt (0);
 					options.ReportFileName = filename;
